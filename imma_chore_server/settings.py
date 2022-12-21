e833fe619c96bafa9711fb5faa660604c4e82f99
Christopher Cialone <ccialone0929@gmail.com>
2022-12-20 20:47:41 -0500

    Registered app in settings.py

--- imma_chore_server/settings.py
+++ imma_chore_server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'imm_a_chore_app.apps.ImmaChoreAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
