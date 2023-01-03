8c047e743e5ec7bf55305c4e24761210e0c7d314
amandaTesterman <102314436+amandaTesterman@users.noreply.github.com>
2023-01-02 19:43:12 -0600

    adding data to parentView

--- imma_chore_app/views.py
+++ imma_chore_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class ParentView():
+    def get(self. request, parent_id):
+
+        kids = Kid.object.all()
+        kid_form = KidForm()
+
+        html_data = {
+            "kid_list" : kids,
+            "form" : kid_form
+        }
+
+        return render(
+            request = request,
+            template_name = 'parent.html',
+            context = html_data,
+        )
+
+
