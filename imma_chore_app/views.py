8bc3572c6965522f499fb7189f9e31f3a2e7b3bb
amandaTesterman <102314436+amandaTesterman@users.noreply.github.com>
2023-01-02 19:56:35 -0600

    changing some info

--- imma_chore_app/views.py
+++ imma_chore_app/views.py
@@ -4,14 +4,17 @@ from django.views import View
 # Create your views here.
 class ParentView():
     def get(self. request, parent_id):
-
-        kids = Kid.object.all()
-        kid_form = KidForm()
-
-        html_data = {
-            "kid_list" : kids,
-            "form" : kid_form
+        parent = Parent.object.all()
+        # kids = Kid.object.all()
+        # kid_form = KidForm()
+        html_data{
+            "parent" : parent
         }
+        
+        # html_data = {
+        #     "kid_list" : kids,
+        #     "form" : kid_form
+        # }
 
         return render(
             request = request,
