ea1c1123d95a11bb601db67183922275e64260e7
amandaTesterman <102314436+amandaTesterman@users.noreply.github.com>
2023-01-03 18:27:25 -0600

    making sure these changes are here

--- imma_chore_app/models.py
+++ imma_chore_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+
+
+
+
+class Kid(model.Model):
+    parent = model.ForeignKey()
+    name = models.CharField(max_length=35)
+    allowance_earned = models.IntegerField()
+
+
+class Chore(model.Model):
